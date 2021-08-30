02a68d1777507324c495cf685671b53ed284bd5c
visheshgargavi <63608636+visheshgargavi@users.noreply.github.com>
2021-08-30 14:44:12 +0530

    Update main.tf

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -1,7 +1,18 @@
+provider "github" {
+  token = var.token
+}
+
 module "ec2" {
      source = "./ec2"
 }
 
-#module "git" {
-#     source = "./git"
-#}
+module "git" {
+     depends_on = [
+          module.ec2
+     ]
+     source = "./github"
+     #repo_url = var.repo_url
+     repo_name = var.repo_name
+     ipdata = module.ec2.ip_data
+     token=var.token
+}
